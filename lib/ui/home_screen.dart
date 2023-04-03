f5638a8d3632594a8d270bf9b2b3c6fa481c1e88
Rahul Kumar <rahulloni000@gmail.com>
2023-04-03 14:41:38 +0545

    fetch deta comp

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_flutter/post/add_post.dart';
 import 'package:firebase_flutter/ui/auth/login_screen.dart';
 import 'package:firebase_flutter/utils/utils.dart';
@@ -13,6 +16,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref("Post");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,8 +46,20 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Center(
-        child: Text("Home Screen"),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: ref,
+              itemBuilder: (context, snapshot, animation, index) {
+                return ListTile(
+                  title: Text(snapshot.child("title").value.toString()),
+                  subtitle: Text(snapshot.child('id').value.toString()),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_rounded),
